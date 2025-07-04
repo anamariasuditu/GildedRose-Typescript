e08f72fba1aacabe126d841ee49017b5a6fccaa4
AnaSud <Ana-Maria.Suditu@softwire.com>
2025-07-04 16:45:55 +0300

    test for AgedBrie

--- test/golden-master-text-test.spec.ts
+++ test/golden-master-text-test.spec.ts
@@ -3,10 +3,26 @@ import { expect } from "chai";
 
 describe("Gilded Rose", () => {
 
-    it("Should not decrease Quality for Sulfuras",() => {
+    it("Should not increase/decrease Quality for Sulfuras",() => {
         const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ]);
         gildedRose.updateQuality();
         expect(gildedRose.items[0].sellIn).to.equal(0);
         expect(gildedRose.items[0].quality).to.equal(80);
     });
+    it("Should increase Quality for AgedBrie with with Quality<50",() => {
+        const gildedRose = new GildedRose([ new Item('Aged Brie', 7, 40) ]);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).to.equal(6);
+        expect(gildedRose.items[0].quality).to.equal(41);
+    });
+    it("Should not increase/decrease for AgedBrie with Quality>50",() => {
+        const gildedRose = new GildedRose([ new Item('Aged Brie', 15, 60) ]);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).to.equal(14);
+        expect(gildedRose.items[0].quality).to.equal(60);
+    });
+
+
+
+
 })
