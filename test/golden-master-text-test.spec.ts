3d253c3dcafc11245061adf7d38a13aab8938d91
AnaSud <Ana-Maria.Suditu@softwire.com>
2025-07-04 17:00:55 +0300

    test for Backstage & edge cases

--- test/golden-master-text-test.spec.ts
+++ test/golden-master-text-test.spec.ts
@@ -21,8 +21,42 @@ describe("Gilded Rose", () => {
         expect(gildedRose.items[0].sellIn).to.equal(14);
         expect(gildedRose.items[0].quality).to.equal(60);
     });
-
-
+    it("Should decrease Quality",() => {
+        const gildedRose = new GildedRose([ new Item('Something', 15, 35) ]);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).to.equal(14);
+        expect(gildedRose.items[0].quality).to.equal(34);
+    });
+    it("Should not decrease Quality<0",() => {
+        const gildedRose = new GildedRose([ new Item('Something', 0, 0) ]);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).to.equal(-1);
+        expect(gildedRose.items[0].quality).to.equal(0);
+    });
+    it("Should increase Quality twice for Backstage if sellin<11",() => {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 10, 32) ]);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).to.equal(9);
+        expect(gildedRose.items[0].quality).to.equal(34);
+    });
+    it("Should increase Quality once for Backstage if sellin>=11",() => {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 11, 32) ]);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).to.equal(10);
+        expect(gildedRose.items[0].quality).to.equal(33);
+    });
+    it("Should set Quality=0 once for Backstage if sellin<0",() => {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', -1, 32) ]);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).to.equal(-2);
+        expect(gildedRose.items[0].quality).to.equal(0);
+    });
+    it("Should set Quality=0 once for Backstage if sellin<0",() => {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', -1, 60) ]);
+        gildedRose.updateQuality();
+        expect(gildedRose.items[0].sellIn).to.equal(-2);
+        expect(gildedRose.items[0].quality).to.equal(0);
+    });
 
 
 })
